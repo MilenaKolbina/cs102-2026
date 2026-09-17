c91c5a1d55bea806a7e43b9c60b8d94b1a944973
MilenaKolbina <milena.kolbina@gmail.com@example.com>
2026-09-17 14:27:21 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,6 +1,6 @@
 """Модуль для возврата сообщения."""
 
+
 def text():
     """Возвращает строку, отсутствовала переменная message."""
-    message = "message"
-    return message
+    return "message"
